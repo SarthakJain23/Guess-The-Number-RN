2c34088992054578aebf349a3139e6696ff787dd
Sarthak Jain <sarthak.jain@turom.co>
2025-11-16 20:00:32 +0530

    Enhance UI with gradient background and new assets; update GameStartScreen styles

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { ImageBackground, StyleSheet } from "react-native";
 import GameStartScreen from "./screens/GameStartScreen";
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
-      <GameStartScreen />
-    </View>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <GameStartScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
